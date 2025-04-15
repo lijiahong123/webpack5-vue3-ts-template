889e123c161e9159fee7afd552a0b55288c8bf91
小李 <1471363285@qq.com>
2025-04-15 22:48:28 +0800

    feat: eslint

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,5 +1,7 @@
 const BaseConfig = require("./webpack.base");
 const { merge } = require("webpack-merge");
+const ESLintPlugin = require("eslint-webpack-plugin");
+const path = require("path");
 /**
  * @type {import('webpack').Configuration}
  * @description webpack开发环境配置
@@ -11,6 +13,15 @@ module.exports = merge(BaseConfig, {
     port: 8080, // 端口号
     // open: true, // 自动打开浏览器
     historyApiFallback: true, // 解决history理由模式下刷新页面404时候，重定向到index.html
-    static: true
+    static: true,
   },
+  plugins: [
+    new ESLintPlugin({
+      extensions: ["ts", "js", "vue", "tsx", "jsx"], // 需要lint的文件后缀名
+      context: path.resolve(__dirname, "../src"), // 需要lint的文件目录
+      exclude: "node_modules", // 排除node_modules目录
+      cache: true, // 开启缓存
+      cacheLocation: path.resolve(__dirname, "../node_modules/.cache/.eslintcache"), // 缓存位置
+    }),
+  ],
 });
--- src/components/Image.vue
+++ src/components/Image.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <img src="@/assets/images/1.webp" alt="1">
+        <img src="@/assets/images/1.webp" alt="1" />
         <img src="@/assets/images/2.webp" alt="2">
         <img src="@/assets/images/3.webp" alt="3">
         <img src="@/assets/images/4.png" alt="4">
@@ -10,7 +10,6 @@
 
 <script setup lang="ts">
 console.log(BASE_URL, "BASE_URL"); // 通过webpackDifinePlugin注入的变量
-
 </script>
 
 <style lang="scss" scoped></style>
